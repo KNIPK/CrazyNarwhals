a692eaf8258452d24a3911999168e5600015a1a3
Michael Apanowicz <michal.apanowicz@gmail.com>
2012-08-23 22:26:20 +0200

    Crazy Narwhals Hello World
    Hello World application. All interesting files are in the catalog
    assets/www. Other files are Android files, because our main target will
    be Android phones.

--- src/com/example/crazynarwhals/MainActivity.java
+++ src/com/example/crazynarwhals/MainActivity.java
@@ -3,13 +3,15 @@ package com.example.crazynarwhals;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import org.apache.cordova.*;
 
-public class MainActivity extends Activity {
+public class MainActivity extends DroidGap {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        super.loadUrl("file:///android_asset/www/index.html");
     }
 
     @Override
